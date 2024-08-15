030a777679b5def6268a09c72ef8dd2cc5af9c04
Sasha <shb.sohrabi@gmail.com>
2024-08-15 15:49:59 +0200

    Loading Data On Startup

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,18 @@
 const http = require('http');
 const app = require('./app');
+const { loadPlanetsData } = require('./models/planets.model');
 const PORT = process.env.PORT || 8080;
 
 const server = http.createServer(app);
 
-// Start the server
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}...`);
-});
+async function startServer() {
+  await loadPlanetsData();
+
+  // Start the server
+  server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}...`);
+  });
+}
+
+
+startServer();
