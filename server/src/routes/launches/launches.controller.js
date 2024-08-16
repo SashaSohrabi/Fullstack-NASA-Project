e37248894705e1d1c9691f905141106f956db6bf
Sasha <shb.sohrabi@gmail.com>
2024-08-16 12:58:34 +0200

    Validation for new launch Post request

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -7,7 +7,19 @@ function httpGetAllLaunches(req, res) {
 function httpAddNewLaunch(req, res) {
   const newLaunch = req.body;
 
+  if (
+    !newLaunch.mission ||
+    !newLaunch.rocket ||
+    !newLaunch.launchDate ||
+    !newLaunch.destination
+  ) {
+    return res.status(400).json({ error: 'Missing required fields' });
+  }
+
   newLaunch.launchDate = new Date(newLaunch.launchDate);
+  if (isNaN(newLaunch.launchDate)) {
+    return res.status(400).json({ error: 'Invalid launch date' });
+  }
 
   addNewLaunch(newLaunch);
   return res.status(201).json(newLaunch);
